c7cc0c2b1ab7341932758c94cef96ee6b9f83942
François Pluchino <francois.pluchino@gmail.com>
2018-12-18 18:18:15 +0100

    Update webpack encore with babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -8,32 +8,25 @@
  */
 
 const Encore = require('@symfony/webpack-encore');
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const server = require('./examples/server');
 
 const config = Encore
     .setOutputPath('build/assets')
     .setPublicPath('/assets')
-    .autoProvidejQuery()
+    .disableSingleRuntimeChunk()
     .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
     .cleanupOutputBeforeBuild()
+    .autoProvidejQuery()
     .enableLessLoader()
     .addEntry('main', './examples/main.js')
-    .addPlugin(new CopyWebpackPlugin([
-        { from: './assets', to: './images', test: /.(webp|jpg|jpeg|png|gif|svg|ico)$/ }
-    ]))
+    .copyFiles({
+        from: './assets',
+        to: './images/[path][name].[ext]',
+        pattern: /.(webp|jpg|jpeg|png|gif|svg|ico)$/
+    })
     .getWebpackConfig()
 ;
-
-// Replace Webpack Uglify Plugin
-if (Encore.isProduction()) {
-    config.plugins = config.plugins.filter(
-        plugin => !(plugin instanceof webpack.optimize.UglifyJsPlugin)
-    );
-    config.plugins.push(new UglifyJsPlugin());
-}
 
 // Config of ajax data
 if (config.devServer) {
